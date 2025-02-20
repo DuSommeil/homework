43dafa9f83c1770b766748594db718266a0f0a64
Maksim-Mac <mqwew07@gmail.com>
2025-02-20 08:02:38 +0300

    20.02.25

--- 12 - CLASS/1.cpp
+++ 12 - CLASS/1.cpp
@@ -1,3 +1,9 @@
+// Класс Электрический счетчик. При создании объекта указывается диапазон значении, 
+// которые может отображать счетчик (при возникновении значения на 1 больше заданного максимального 
+// счетчик "сбрасывается" на 0), единицы измерения. Для счетчика должны быть определены 
+// операции увеличения значения на К и печать текущего значения.  
+// В функции main проверить эти методы.
+
 #include <iostream>
 #include <string>
 
@@ -5,7 +11,7 @@ using namespace std;
 
 class ElectricMeter
 {
-private:
+// задаем поля класса
     int minValue;          
     int maxValue;          
     string unit;
@@ -15,8 +21,8 @@ public:
     // конструктор с параметрами
     ElectricMeter(int minVal, int maxVal, string unit): minValue(minVal), maxValue(maxVal), unit(unit), currentValue(minVal) {}
 
-    // увеличиваем нынешнее значение на k учитывая переход за предел диапазона
-    void increaseBy(int k)
+    // увеличиваем нынешнее значение на k учитывая переход за предел диапазона (если таковой случается)
+    void increase(int k)
     {
         currentValue += k;
         if (currentValue > maxValue)
@@ -35,19 +41,19 @@ public:
 int main() 
 {
     // создаем счетчик с минимальным и максимальным значением, а также единицой измерения (киловатт в час)
-    ElectricMeter meter(0, 999, "kWh");
+    ElectricMeter schet(0, 999, "kWh");
 
     // увеличиваем значение на 100
-    meter.increaseBy(100);
-    meter.printCurrentValue();
+    schet.increase(100);
+    schet.printCurrentValue();
 
     // увеличиваем значение на 900
-    meter.increaseBy(900);
-    meter.printCurrentValue();  // перезапус т.к больше 999
+    schet.increase(900);
+    schet.printCurrentValue();  // перезапуск т.к больше 999
 
     // увеличиваем значение на 500
-    meter.increaseBy(500);
-    meter.printCurrentValue(); 
+    schet.increase(500);
+    schet.printCurrentValue(); // в итоге на выводе в конце будет 500
 
     return 0;
 }
