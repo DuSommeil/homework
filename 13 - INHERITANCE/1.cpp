7aa61d88191b9a69c27bae23314356e3298d0e7d
Maksim-Mac <mqwew07@gmail.com>
2025-03-06 09:31:59 +0300

    06.03

--- 13 - INHERITANCE/1.cpp
+++ 13 - INHERITANCE/1.cpp
@@ -1,3 +1,10 @@
+// Создать класс Окружность, член класса – R. 
+// Предусмотреть в классе методы вычисления и вывода сведений о фигуре – площади, длины окружности. 
+// Создать производный класс – Прямой цилиндр с высотой h, добавить в класс метод 
+// определения объема фигуры, перегрузить методы расчета площади и вывода сведении о фигуре. 
+// Написать программу, демонстрирующую работу с классом: дано X окружностей и М цилиндров, 
+// найти окружность максимальной площади и средний объем цилиндров.
+
 #include <iostream>
 #include <cmath>
 
@@ -9,19 +16,19 @@ protected:
     double radius;
 
 public:
-    Circle(double r) : radius(r) {}
+    Circle(double r) : radius(r) {} // конструктор с параметром
 
-    double Length() const
+    double Length() const //вычислиение длины окружности
     {
         return 2 * M_PI * radius;
     }
 
-    virtual double Area() const
+    virtual double Area() const //вычисление площади окружности
     {
         return M_PI * radius * radius;
     }
 
-    virtual void Print() const
+    virtual void Print() const //вывод информации окружности
     {
         cout << "Окружность с радиусом: " << radius << endl;
         cout << "Площадь: " << Area() << endl;
@@ -31,7 +38,7 @@ public:
     ~Circle(){}
 };
 
-class Cylinder : public Circle
+class Cylinder : public Circle //производный класс цилиндр
 {
 private:
     double height;
@@ -41,15 +48,15 @@ public:
 
     double Volume() const
     {
-        return Area() * height;
+        return Area() * height; //вычисление объема
     }
 
     double Area() const
     {
-        return 2 * M_PI * radius * (radius + height); // перегрузка метода для вычисления площади цилиндра
+        return 2 * M_PI * radius * (radius + height); // перегрузка метода для вычисления площади цилиндра (переопределили)
     }
 
-    void Print() const
+    void Print() const //вывод значений окружности
     {
         cout << "Прямой цилиндр с высотой " << height << ", а радиусом " << radius << endl;
         cout << "Площадь: " << Area() << endl;
@@ -67,9 +74,10 @@ int main()
     cout << "Введите кол-во цилиндров: ";
     cin >> M;
 
-    Circle* circles[X];
-    Cylinder* cylinders[M];
+    Circle* circles[X]; // массив указателей на объекты класса Circle
+    Cylinder* cylinders[M]; // массив указателей на объекты класса Cylinder
 
+    // вводим данные для окружностей
     for (int i = 0; i < X; i++)
     {
         double radius;
@@ -78,6 +86,7 @@ int main()
         circles[i] = new Circle(radius);
     }
 
+    //ввод данных для цилиндров
     for (int i = 0; i < M; i++)
     {
         double radius, height;
@@ -86,6 +95,7 @@ int main()
         cylinders[i] = new Cylinder(radius, height);
     }
 
+    // поиск с максимальной плозадью
     double maxCircleArea = 0;
     int maxCircleIndex = -1;
     for (int i = 0; i < X; i++)
@@ -97,18 +107,19 @@ int main()
         }
     }
 
+    // вывод окружности с максимальной площадью
     if (maxCircleIndex != -1)
     {
         cout << "\n\nОкружность с максимальной площадью является:\n";
         circles[maxCircleIndex]->Print();
     }
-    
+
+    // вычисление среднего объема цилиндров
     double totalVolume = 0;
     for (int i = 0; i < M; i++)
     {
         totalVolume += cylinders[i]->Volume();
     }
-
     double averageVolume;
     if (M > 0) 
     {
@@ -123,12 +134,12 @@ int main()
 
     for (int i = 0; i < X; i++)
     {
-        delete circles[i];
-    }
+        delete circles[i]; 
+    } // освобождаем память
     for (int i = 0; i < M; i++)
     {
         delete cylinders[i];
-    }
+    } // освобождаем память
 
     return 0;
 }
